6997a952542acb6e68be772dd4bfcd32b3861181
San Gillis <gillis.san@gmail.com>
2013-07-08 14:12:33 +0200

    wallpaper_changer.py

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -5,7 +5,7 @@ Changes wallpaper to some random picture in the given directory.
 import os
 from random import choice
 
-picture_dir = '/home/san/Pictures/Wallpapers/'
+picture_dir = '/home/san/Pictures/nature_wallpapers/'
 
 def change_wallpaper(directory):
   """Changes the current wallpaper to a random wallpaper in directory"""
@@ -15,7 +15,6 @@ def change_wallpaper(directory):
   file_path = 'file://' + directory + choice(wallpapers)
   # Create the bash command to change the wallpaper
   command = 'gsettings set org.gnome.desktop.background picture-uri ' + file_path
-  print command
   # Execute the command
   os.system(command)
 
