ddbf4aaafc623f0c8a3bd62605f2833c5a174b5e
San Gillis <gillis.san@gmail.com>
2014-04-05 17:15:05 +0200

    dzen support

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.DynamicLog
 
-main = do
-    xmonad $ defaultConfig {
-        modMask = mod4Mask
-        } `additionalKeys`
-        [ ((mod4Mask, xK_x), spawn "dmenu_run")
-        ]
+main = xmonad =<< dzen myConfig
+
+myConfig = defaultConfig
+    { modMask = mod4Mask
+    } `additionalKeys`
+    [ ((mod4Mask, xK_x), spawn "dmenu_run")
+    ]
