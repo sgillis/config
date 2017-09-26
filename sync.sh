a712c337835ce26b068d8c87d72bf1d23eb4bb95
San Gillis <gillis.san@gmail.com>
2017-09-26 14:43:57 +0200

    Generate log file based on datetime

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-/usr/local/bin/aws s3 sync /Users/sgillis/S3 s3://sgillis
+TODAY=`date +%Y-%m-%dT%H:%M:%S`
+LOG="/Users/sgillis/log/sync$TODAY.log"
+/usr/local/bin/aws s3 sync /Users/sgillis/S3 s3://sgillis > $LOG 2>&1
